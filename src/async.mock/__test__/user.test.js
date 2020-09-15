bfbbc2de929f11da820d8ce9a11b12627584bc28
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-15 23:04:04 +0800

    [ruiling] feat: finish homework(forget commit)

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { register } from "../user";
+import { verifyUsername, verifyPassword } from "../verify";
 
 jest.mock("../verify");
 jest.mock("axios");
@@ -6,9 +8,15 @@ jest.mock("axios");
 describe("register", () => {
   test("should post user when validated", () => {
     // TODO 19: add test here
+    axios.post.mockResolvedValue({});
+    register("test", "test");
+    expect(axios.post).toHaveBeenCalled();
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyPassword.mockReturnValueOnce(false);
+    verifyUsername.mockReturnValueOnce(false);
+    await expect(register("test", "test")).rejects.toThrowError();
   });
 });
--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,14 @@
+import axios from "axios";
+import getUsers from "../users";
+
+jest.mock("axios");
+
+
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
+  test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
+    const user = { data: "test" };
+    axios.get.mockResolvedValue(user);
+    await expect(getUsers()).resolves.toEqual("test");
   });
 });
